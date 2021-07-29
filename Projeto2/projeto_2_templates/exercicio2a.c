7c63b7e802f2907f1161385f92a54e925229e6d9
Felipe <f.tommaselli@usp.br>
2021-07-29 11:51:27 -0300

    Projeto2: Hashing fechado funcionando 100% e comentado

--- Projeto2/projeto_2_templates/exercicio2a.c
+++ Projeto2/projeto_2_templates/exercicio2a.c
@@ -65,39 +65,33 @@ unsigned h_mul(unsigned x, unsigned i, unsigned B){
 }
 
 void criar(hash *tabela, unsigned B){
-    int i;
 
     tabela->vet = (string *) calloc(B, sizeof(string));
-
-    for(i = 0; i < B; i++){
-        tabela->vet[i] = "-";
-    }
 }
 
-int inserir(hash *tabela, unsigned *colisoes, unsigned B, string e, unsigned (*fHash)(unsigned, unsigned, unsigned)){
+int inserir(hash *tabela, unsigned *colisoes, unsigned B, string e, unsigned ( *fHash)(unsigned, unsigned, unsigned)){
     
     unsigned pos, i, aux;
 
     aux = converter(e);
     
+
     for(i = 0; i < B; i++){
 
         pos = fHash(aux, i, B);
 
         //if(i == 1)
-        //    (*colisoes)++;
+        //    ( *colisoes)++;
 
-        if(((int) strcmp(tabela->vet[pos], "-")) == 0){ 
-            tabela->vet[pos] = calloc(MAX_STRING_LEN, sizeof(string));
+        if(tabela->vet[pos] == NULL){
+            tabela->vet[pos] = (string) malloc(MAX_STRING_LEN*sizeof(char));
             strcpy(tabela->vet[pos], e);
-            //return pos;
             return i;
         }
         
-        if(strcmp(tabela->vet[pos], e) == 0)
+        if(!strcmp(tabela->vet[pos], e))
             return -1;
 
-        //(*colisoes)++;
     }
     return -1;
 }
@@ -111,25 +105,28 @@ int buscar(hash *tabela, unsigned B, string e, unsigned (*fHash)(unsigned, unsig
 
         pos = fHash(aux, i, B);
 
-        if(strcmp(tabela->vet[pos], e) == 0)
-            return pos;
-        if(tabela->vet[pos] == "-")
+        if(tabela->vet[pos] == NULL)
             return -1;
+            
+        if((strcmp(tabela->vet[pos], e)) == 0)
+            return pos;
+
     }
     return -1;
 }
 
 void limpar(hash *tabela, unsigned B){
-    int pos;
+    int i;
 
-    tabela->vet = (string *) calloc(B, sizeof(string));
-
-    for(pos = 0; pos < B; pos++){
-        tabela->vet[pos] = "-";
-    }
+    for(i = 0; i < B; i++)
+        free(tabela->vet[i]);
+    
+    free(tabela->vet);
 }
 
 int main(int argc, char const *argv[]){
+    //DECLARAÇÕES E PRÉ DEFINIÇÕES
+
     unsigned N = 50000;
     unsigned M = 70000;
     unsigned B = 150001;
@@ -143,49 +140,90 @@ int main(int argc, char const *argv[]){
     string* insercoes = ler_strings("strings_entrada.txt", N);
     string* consultas = ler_strings("strings_busca.txt", M);
 
+    // HASHING FECHADO
+
+    // cria uma tabela do tipo hash(com um vetor de tamanho B)
     hash tabela;
+
+    //* HASH POR DIVISÃO
 
     // cria tabela hash com hash por divisão
     criar(&tabela, B);
 
     // inserção dos dados na tabela hash usando hash por divisão
+    
+    // inicia contagem do tempo
     inicia_tempo();
-    for (int i = 0; i < N; i++){
-        if(inserir(&tabela, &colisoes_h_div, B, insercoes[i], &h_div) > 0); // inserir insercoes[i] na tabela hash
+    // roda o vetor de entradas (tamanho N)
+    for (int i = 0; i < N; i++)
+        // inserir insercoes[i] na tabela hash utilizando hash por divisao
+        // caso a funcao insserir retorne i > 0, houve ao menos uma colisão  
+        if(inserir(&tabela, &colisoes_h_div, B, insercoes[i], &h_div) > 0)
             colisoes_h_div++;    
-    }
+    
+    // finaliza contagem de tempo
     double tempo_insercao_h_div = finaliza_tempo();
 
     // consulta dos dados na tabela hash usando hash por divisão
+
+    // inicia contagem do tempo
     inicia_tempo();
-    for (int i = 0; i < M; i++){
-        if(buscar(&tabela, B, consultas[i], &h_div) != -1) // buscar consultas[i] na tabela hash
-            encontrados_h_div++;// buscar consultas[i] na tabela hash
-    }
+    // roda o vetor de consultas para a busca (tamanho M)
+    for (int i = 0; i < M; i++)
+        // busca consultar[i] na tabela hash
+        // caso retorne -1 elemento não encontrado, cc elemento encontrado
+        if(buscar(&tabela, B, consultas[i], &h_div) != -1)
+            encontrados_h_div++;
 
+    // finaliza contagem de tempo
     double tempo_busca_h_div = finaliza_tempo();
-
-    // limpa a tabela hash com hash por divisão
+        
+    // limpa e libera o espaço de memória da tabela hash por divisão
     limpar(&tabela, B);
+    
+    //* HASH POR MULTIPLICAÇÃO
+
+    // cria tabela hash com hash por multiplicaçao
+    criar(&tabela, B);
 
     // inserção dos dados na tabela hash usando hash por multiplicação
+
+    // inicia contagem do tempo
     inicia_tempo();
-    for (int i = 0; i < N; i++){
-        if(inserir(&tabela, &colisoes_h_mul, B, insercoes[i], &h_mul) > 0); // inserir insercoes[i] na tabela hash
+    // roda o vetor de entradas (tamanho N)
+    for (int i = 0; i < N; i++)
+        // inserir insercoes[i] na tabela hash utilizando hash por multiplicacao
+        // caso a funcao insserir retorne i > 0, houve ao menos uma colisão  
+        if(inserir(&tabela, &colisoes_h_mul, B, insercoes[i], &h_mul) > 0) 
             colisoes_h_mul++;
-    }
+    
+    // finaliza contagem de tempo
     double tempo_insercao_h_mul = finaliza_tempo();
 
+
     // busca dos dados na tabela hash com hash por multiplicação
+    
+    // inicia contagem do tempo
     inicia_tempo();
-    for (int i = 0; i < M; i++){
+
+    // roda o vetor de consultas para a busca (tamanho M)
+    for (int i = 0; i < M; i++)
+        // busca consultar[i] na tabela hash
+        // caso retorne -1 elemento não encontrado, cc elemento encontrado
         if(buscar(&tabela, B, consultas[i], &h_mul) != -1) // buscar consultas[i] na tabela hash
             encontrados_h_mul++;
-    }
+    
+    // finaliza contagem de tempo
     double tempo_busca_h_mul = finaliza_tempo();
 
-    // limpa a tabela hash com hash por multiplicação
+    // limpa e libera o espaço de memória da tabela hash por multiplicacao
     limpar(&tabela, B);
+
+    // liberacao do espaco de memoria alocado do vetor de insercoes e consultas
+    free(insercoes);
+    free(consultas);
+
+    // OUTPUTS DOS DADOS OBTIDOS
 
     printf("Hash por Divisão\n");
     printf("Colisões na inserção: %d\n", colisoes_h_div);
@@ -199,8 +237,5 @@ int main(int argc, char const *argv[]){
     printf("Tempo de busca      : %fs\n", tempo_busca_h_mul);
     printf("Itens encontrados   : %d\n", encontrados_h_mul);
 
-    free(insercoes);
-    free(consultas);
-
-    return 0;
+    return 0; // sucesso
 }
